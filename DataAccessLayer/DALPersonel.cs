8ff8111bace08bb70eb819e289e1463d585def15
seferogluemre <yunusemre5331@icloud.com>
2025-01-11 15:25:59 +0300

    entity department object created data access layer layer created listing function written

---
+++ DataAccessLayer/DALPersonel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccessLayer
+{
+    class DALPersonel
+    {
+        public static List<>
+
+
+    }
+}
---
+++ EntityLayer/EntityDepartman.cs
@@ -0,0 +1,13 @@
+﻿namespace EntityLayer
+{
+    public class EntityDepartman
+    {
+        private int id;
+        private string ad;
+        private string aciklama;
+
+        public string Aciklama { get => aciklama; set => aciklama = value; }
+        public string Ad { get => ad; set => ad = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
