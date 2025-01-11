3a0c25ded8a1c7a91f36f5259feb6a5d30c9a20b
seferogluemre <yunusemre5331@icloud.com>
2025-01-11 15:16:52 +0300

    Data access layer created and sql connection class created

---
+++ DataAccessLayer/Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Connection
+    {
+        public SqlConnection connection()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-4DMQSS4;Initial Catalog=DbPersonel;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+            baglanti.Open();
+            return baglanti;
+        }
+    }
+}
