d1690639acf5fc8cd5dfd668c2bacb1d4af6568b
seferogluemre <yunusemre5331@icloud.com>
2025-01-11 15:07:15 +0300

    We defined properties and configured get set blocks by encapsulation

--- EntityLayer/EntityPersonel.cs
+++ EntityLayer/EntityPersonel.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EntityLayer
+﻿namespace EntityLayer
 {
-    internal class EntityPersonel
+    public class EntityPersonel
     {
+        private int id;
+        private string ad;
+        private string soyad;
+        private string sehir;
+        private string görev;
+        private short maas;
 
+        public int Id { get => id; set => id = value; }
+        public string Ad { get => ad; set => ad = value; }
+        public string Soyad { get => soyad; set => soyad = value; }
+        public string Sehir { get => sehir; set => sehir = value; }
+        public string Görev { get => görev; set => görev = value; }
+        public short Maas { get => maas; set => maas = value; }
     }
 }
